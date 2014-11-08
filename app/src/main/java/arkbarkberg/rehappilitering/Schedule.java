6b085a4b5ec7aec3e0fef1146789936946d898d1
Caroline Arkenson <arkenson@kth.se>
2014-11-08 19:29:18 +0100

    Merged YourExercises with Navigation Bar

--- app/src/main/java/arkbarkberg/rehappilitering/Schedule.java
+++ app/src/main/java/arkbarkberg/rehappilitering/Schedule.java
@@ -19,7 +19,7 @@ public class Schedule extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        return inflater.inflate(R.layout.activity_view_schedule, container, false);
     }
 
 }

40d0e5bb3d7786a8f5c939258a24a2772d284384
mbark <barksten@kth.se>
2014-11-08 19:58:25 +0100

    Also save values

--- app/src/main/java/arkbarkberg/rehappilitering/ExerciseCategory.java
+++ app/src/main/java/arkbarkberg/rehappilitering/ExerciseCategory.java
@@ -6,9 +6,13 @@ import java.util.ArrayList;
  * Created by mbark on 08/11/14.
  */
 public class ExerciseCategory {
+    private int id;
+    private String name;
     private ArrayList<Exercise> exercises;
 
     public ExerciseCategory(int id, String name) {
+        this.id = id;
+        this.name = name;
         exercises = new ArrayList<Exercise>();
     }
 

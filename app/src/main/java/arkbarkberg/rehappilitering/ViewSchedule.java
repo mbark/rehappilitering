1bf72eb61592a1e8943368929c4012524adefc30
mbark <barksten@kth.se>
2014-11-08 17:46:21 +0100

    Add prototype for the schedule

--- app/src/main/java/arkbarkberg/rehappilitering/ViewSchedule.java
+++ app/src/main/java/arkbarkberg/rehappilitering/ViewSchedule.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 //Visar upplägg
 

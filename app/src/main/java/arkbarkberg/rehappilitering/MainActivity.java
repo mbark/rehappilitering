1b9cef1a41dbf692bfec4d09bd70c36f69cad794
Caroline Arkenson <arkenson@kth.se>
2014-11-08 13:08:01 +0100

    Start a shell

--- app/src/main/java/arkbarkberg/rehappilitering/MainActivity.java
+++ app/src/main/java/arkbarkberg/rehappilitering/MainActivity.java
@@ -4,14 +4,35 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button maddButton;
+    Button mscheduleButton;
+    Button mstatisticsButton;
+    Button mexerciseButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        maddButton = (Button)findViewById(R.id.addButton);
+        maddButton.setOnClickListener(this);
+
+       mscheduleButton = (Button)findViewById(R.id.scheduleButton);
+       mscheduleButton.setOnClickListener(this);
+
+        mstatisticsButton = (Button)findViewById(R.id.statisticsButton);
+        mstatisticsButton.setOnClickListener(this);
+
+        mexerciseButton = (Button)findViewById(R.id.exerciseButton);
+        mexerciseButton.setOnClickListener(this);
+
     }
 
 
@@ -20,6 +41,38 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v){
+
+        switch(v.getId()) {
+            case R.id.addButton:
+
+                //Skicka till skärm "Lägg till övning"
+
+            break;
+
+            case R.id.scheduleButton:
+
+                //Skicka till skärm "Ditt rehabschema"
+
+            break;
+
+            case R.id.statisticsButton:
+
+                //Skicka till skärm "Din rehabstatistik"
+
+            break;
+
+            case R.id.exerciseButton:
+
+                //Skicka till skärm "Dina rehabområden"
+
+            break;
+
+        }
+
     }
 
     @Override
